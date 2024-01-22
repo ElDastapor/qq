b6971a29dd72f43d63d29559ce1d57a46b07a83d
ElDastapor <157402983+ElDastapor@users.noreply.github.com>
2024-01-22 21:27:41 +0300

    Create TT.java

---
+++ TT.java
@@ -0,0 +1,25 @@
+package Time;
+import java.util.Date;
+public class TT {
+    public static void main(String[] args) {
+        // СМЕЩЕНИЕ
+        Date d = new Date();
+        int i = d.getTimezoneOffset();
+        System.out.println(i);
+
+        System.out.println(d.toGMTString());
+
+        System.out.println(d.toLocaleString());
+
+        System.out.println(d.toString());
+
+        Date с = new Date();
+
+        System.out.println(d.compareTo(с));
+        System.out.println(d.compareTo(с));
+
+    }
+
+
+
+}
